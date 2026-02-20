6be85e71e38555c1b8eacc3d8552ea19d27c08cf
hazeljackie <gitongajacqueline3@gmail.com>
2026-02-20 14:01:53 +0000

    Add Units class to display offered courses

---
+++ Units.java
@@ -0,0 +1,14 @@
+// JACQUELINE GITONGA
+// CT100/G/21937/24
+
+public class Units {
+    public static void main(String[] args) {
+
+        System.out.println("Units Offered This Semester:");
+        System.out.println("1. Introduction to Programming");
+        System.out.println("2. Data Structures");
+        System.out.println("3. Computer Networks");
+        System.out.println("4. Database Systems");
+        System.out.println("5. Software Engineering");
+    }
+}
