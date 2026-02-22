2cf4377ac02e2e514e8c96d546962172e120f791
hazeljackie <gitongajacqueline3@gmail.com>
2026-02-23 00:56:06 +0300

    Rename variable 'input' to 'Input' in Cylinder class

--- Cylinder.java
+++ Cylinder.java
@@ -1,3 +1,5 @@
+
+
 // JACQUELINE GITONGA
 //CT100/G/21937/24
 
@@ -6,13 +8,13 @@ import java.util.Scanner;
 public class Cylinder {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        Scanner Input = new Scanner(System.in);
 
         System.out.print("Enter radius: ");
-        double radius = input.nextDouble();
+        double radius = Input.nextDouble();
 
         System.out.print("Enter height: ");
-        double height = input.nextDouble();
+        double height = Input.nextDouble();
 
         double volume = Math.PI * radius * radius * height;
         double surfaceArea = 2 * Math.PI * radius * (radius + height);
@@ -20,6 +22,6 @@ public class Cylinder {
         System.out.println("Volume: " + volume);
         System.out.println("Surface Area: " + surfaceArea);
 
-        input.close();
+        Input.close();
     }
 }
