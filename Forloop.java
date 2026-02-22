b74b5488576832187c277ed720c379908413c873
hazeljackie <gitongajacqueline3@gmail.com>
2026-02-23 01:20:59 +0300

    Add Forloop class to print numbers from 100 to 50

---
+++ Forloop.java
@@ -0,0 +1,14 @@
+//JACQUELINE GITONGA
+//CT100/G/21937/24
+
+public class Forloop {
+
+    public static void main(String[] args) {
+
+        System.out.println("Numbers from 100 down to 50:");
+
+        for (int i = 100; i >= 50; i--) {
+            System.out.println(i);
+        }
+    }
+}
